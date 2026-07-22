94083a7e5180a70907a4b9244efed5d8319fa477
Jemberry <2425770361@qq.com>
2026-07-23 01:39:28 +0800

    Add:Day02_files

---
+++ CloudDisk-v1/main.cc
@@ -0,0 +1,31 @@
+#include "CloudDiskServer.h"
+#include <iostream>
+#include <signal.h>
+
+using namespace std;
+
+WFFacilities::WaitGroup waitGroup(1);
+
+void sig_handler(int)
+{
+    waitGroup.done();
+}
+
+int main()
+{
+	srand(time(NULL));
+    signal(SIGINT, sig_handler);
+
+    CloudDiskServer server;
+
+    // 注册路由
+    server.register_routes();
+
+    if (server.start(8888) == 0) {
+        server.list_routes();
+        waitGroup.wait();
+        server.stop();
+    } else {
+        cerr << "Error: Server start FAILED!" << endl;
+    }
+}
